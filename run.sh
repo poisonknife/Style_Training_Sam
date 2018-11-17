065ecb872dc4d50f80a39accc1786bf1fcf9df72
poisonknife <32144108+poisonknife@users.noreply.github.com>
2018-11-17 10:12:54 -0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+export PYTHONUNBUFFERED=0
+python style.py --style images/aldunate.jpg \
+  --checkpoint-dir checkpoints/ \
+  --vgg-path /styletransfer/data/imagenet-vgg-verydeep-19.mat \
+  --train-path /datasets/coco/coco_train2014 \
+  --model-dir /artifacts \
+  --test images/violetaparra.jpg \
+  --test-dir tests/ \
+  --content-weight 1.5e1 \
+  --checkpoint-iterations 1000 \
+  --batch-size 20
